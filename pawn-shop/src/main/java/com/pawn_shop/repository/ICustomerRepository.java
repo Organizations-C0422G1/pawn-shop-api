17effb27eb859b7bb7e6a0bb641cac598df011e6
TaiNhut231094 <anhtai231094@gmail.com>
2022-10-15 08:31:57 +0700

    TaiLA up

--- pawn-shop/src/main/java/com/pawn_shop/repository/ICustomerRepository.java
+++ pawn-shop/src/main/java/com/pawn_shop/repository/ICustomerRepository.java
@@ -2,9 +2,6 @@ package com.pawn_shop.repository;
 
 import com.pawn_shop.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Long> {
 }
