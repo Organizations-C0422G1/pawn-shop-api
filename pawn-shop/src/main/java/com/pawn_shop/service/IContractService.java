9a0bc47ede64f3a18d2516476dda5f509a2eb64d
TaiNhut231094 <anhtai231094@gmail.com>
2022-10-17 06:13:05 +0700

    TaiLA update

--- pawn-shop/src/main/java/com/pawn_shop/service/IContractService.java
+++ pawn-shop/src/main/java/com/pawn_shop/service/IContractService.java
@@ -4,12 +4,8 @@ import com.pawn_shop.dto.projection.ContractDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
-
 public interface IContractService {
     Page<ContractDto> getAllContractPaginationAndSearch(Pageable pageable, String code, String customerName, String pawnItem, String startDate);
-
-    Optional<ContractDto> getExpiredContractsById(long id);
 
     void returnItem(long id);
 }
