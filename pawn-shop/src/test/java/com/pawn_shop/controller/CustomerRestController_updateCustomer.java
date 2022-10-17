7046be1843e4291496a118440a4f4ffb0425e201
DaPhuc2710 <daphuc2000@gmail.com>
2022-10-17 19:18:23 +0700

    up

--- pawn-shop/src/test/java/com/pawn_shop/controller/CustomerRestController_updateCustomer.java
+++ pawn-shop/src/test/java/com/pawn_shop/controller/CustomerRestController_updateCustomer.java
@@ -3,7 +3,6 @@ package com.pawn_shop.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pawn_shop.dto.CustomerDTO;
 import com.pawn_shop.model.address.Address;
-import com.pawn_shop.model.customer.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,10 +15,9 @@ import java.time.LocalDate;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CustomerRestController_createCustomer {
+public class CustomerRestController_updateCustomer {
     @Autowired
     private MockMvc mockMvc;
 
@@ -35,7 +33,7 @@ public class CustomerRestController_createCustomer {
      */
 
     @Test
-    public void createCustomer_name_14() throws Exception {
+    public void updateCustomer_name_20() throws Exception {
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("");
         customerDTO.setDateOfBirth(LocalDate.parse("2022-12-12"));
@@ -65,7 +63,7 @@ public class CustomerRestController_createCustomer {
      */
 
     @Test
-    public void createCustomer_name_15() throws Exception {
+    public void updateCustomer_name_21() throws Exception {
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("@#$");
         customerDTO.setDateOfBirth(LocalDate.parse("2022-12-12"));
@@ -94,7 +92,7 @@ public class CustomerRestController_createCustomer {
      *
      * @throws Exception this function to test validation of field name successfully
      */
-    public void createCustomer_name_18() throws Exception {
+    public void updateCustomer_name_24() throws Exception {
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Nguyễn Hoàng Đa Phúc");
         customerDTO.setDateOfBirth(LocalDate.parse("2022-12-12"));
@@ -124,7 +122,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field image more specific is Empty
      */
     @Test
-    public void createCustomer_image_14() throws Exception {
+    public void updateCustomer_image_20() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -155,7 +153,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field image more can't access the link
      */
     @Test
-    public void createCustomer_image_15() throws Exception {
+    public void updateCustomer_image_21() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -187,7 +185,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function use to test validation of field image more specific is successfully
      */
     @Test
-    public void createCustomer_image_18() throws Exception {
+    public void updateCustomer_image_24() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -219,7 +217,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field date of birth more specific is successfully
      */
     @Test
-    public void createCustomer_dateOfBirth_18() throws Exception {
+    public void updateCustomer_dateOfBirth_24() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -251,7 +249,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function use to test validation of field date of birth is empty
      */
     @Test
-    public void createCustomer_dateOfBirth_14() throws Exception {
+    public void updateCustomer_dateOfBirth_20() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -283,7 +281,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function use to test validation of field date of birth has age <18
      */
     @Test
-    public void createCustomer_dateOfBirth_16() throws Exception {
+    public void updateCustomer_dateOfBirth_22() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -315,7 +313,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function use to test validation of field date of birth has age  >120
      */
     @Test
-    public void createCustomer_dateOfBirth_17() throws Exception {
+    public void updateCustomer_dateOfBirth_23() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -347,7 +345,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field Phone number is empty
      */
     @Test
-    public void createCustomer_phoneNumber_14() throws Exception {
+    public void updateCustomer_phoneNumber_20() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -379,7 +377,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field Phone number is contain words or special characters
      */
     @Test
-    public void createCustomer_phoneNumber_15() throws Exception {
+    public void updateCustomer_phoneNumber_21() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -411,7 +409,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function use to test validation of field Phone number is successfully
      */
     @Test
-    public void createCustomer_phoneNumber_18() throws Exception {
+    public void updateCustomer_phoneNumber_24() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -443,7 +441,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field id card is empty
      */
     @Test
-    public void createCustomer_idCard_14() throws Exception {
+    public void updateCustomer_idCard_20() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -475,7 +473,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field id card is wrong format
      */
     @Test
-    public void createCustomer_idCard_15() throws Exception {
+    public void updateCustomer_idCard_21() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -507,7 +505,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field id card is <9 numbers
      */
     @Test
-    public void createCustomer_idCard_16() throws Exception {
+    public void updateCustomer_idCard_22() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -539,7 +537,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function to test validation of field id card is > 9 numbers
      */
     @Test
-    public void createCustomer_idCard_17() throws Exception {
+    public void updateCustomer_idCard_23() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -571,7 +569,7 @@ public class CustomerRestController_createCustomer {
      * @throws Exception this function  to test validation of field id card is successfully
      */
     @Test
-    public void createCustomer_IdCard_18() throws Exception {
+    public void updateCustomer_idCard_24() throws Exception {
 
         CustomerDTO customerDTO = new CustomerDTO();
         customerDTO.setName("Hùng Dũng");
@@ -594,5 +592,4 @@ public class CustomerRestController_createCustomer {
                 .andDo(print())
                 .andExpect(status().is2xxSuccessful());
     }
-
 }
