479f2d2907b84f53ef0347ce6ecefe474183b617
Your Name <you@example.com>
2022-10-17 21:15:44 +0700

    update

--- pawn-shop/src/test/java/com/pawn_shop/controller/NewsRestController_getListNews.java
+++ pawn-shop/src/test/java/com/pawn_shop/controller/NewsRestController_getListNews.java
@@ -39,21 +39,35 @@ public class NewsRestController_getListNews {
                 .andExpect(jsonPath("content[3].content").value("Ô tô là một trong những phương tiện được cầm cố nhiều, bởi đây là tài sản lớn và dễ quy đổi thanh khoản vay. Nếu bạn đang gặp vấn đề về tài chính, cần hỗ trợ khoản tiền lớn và đồng thời đang sở hữu xe ô tô BMW, việc cầm xe ô tô"))
                 .andExpect(jsonPath("content[3].postingDay").value("2022-10-10"));
     }
-
     @Test
-    public void getAllNews_7() throws Exception {
+    public void getAllNews_1() throws Exception {
         this.mockMvc.perform(
-                MockMvcRequestBuilders.get("/list-news")
-        ).andDo(print())
-                .andExpect(status().is4xxClientError())
-                .andExpect(jsonPath("totalPages").value(4))
-                .andExpect(jsonPath("totalElements").value(20))
-                .andExpect(jsonPath("content[3].title").value("Cầm xe ô tô BMW Model X3, X4, X5, X7 330i, 340i, 730i, 750i"))
-                .andExpect(jsonPath("content[3].content").value("Ô tô là một trong những phương tiện được cầm cố nhiều, bởi đây là tài sản lớn và dễ quy đổi thanh khoản vay. Nếu bạn đang gặp vấn đề về tài chính, cần hỗ trợ khoản tiền lớn và đồng thời đang sở hữu xe ô tô BMW, việc cầm xe ô tô"))
-                .andExpect(jsonPath("content[3].postingDay").value("2022-10-10"))
-                .andExpect(jsonPath("status").value(true))
-                .andExpect(jsonPath("appUser").value("null"));
+                        MockMvcRequestBuilders.get("/list-news?title=null")
+                ).andDo(print())
+                .andExpect(status().is(404));
     }
+    @Test
+    public void getAllNews_2() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/list-news?title= ")
+                ).andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+    @Test
+    public void getAllNews_3() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/list-news?tile=ádsa")
+                ).andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void getAllNews_4() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.get("/list-news?tile=Cầm Xe Ô Tô Hạng Sang Siêu Xe")
+                ).andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
 
 
 
