6365c6ae680aa58c3ec0b1b6b62ee8d1430c395a
quynhtran11061999 <quynhtran11061999@gmail.com>
2022-10-17 16:07:05 +0700

    update employee-QuynhTX-17/10

--- pawn-shop/src/main/java/com/pawn_shop/repository/IEmployeeRepository.java
+++ pawn-shop/src/main/java/com/pawn_shop/repository/IEmployeeRepository.java
@@ -2,6 +2,28 @@ package com.pawn_shop.repository;
 
 import com.pawn_shop.model.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Transactional
 public interface IEmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query(value = "select id, address,code, date_of_birth,email,gender,img_url,`name`,phone_number,salary,`status`,id_card \n" +
+            "\tfrom employee \n" +
+            "\twhere id = :id", nativeQuery = true)
+    Optional<Employee> findById(@Param("id") Long id);
+
+    @Modifying
+    @Query(value = "update employee set address = :address, date_of_birth = :dateOfBirth,email = :email," +
+            " gender = :gender , img_url = :imgUrl , `name` = :name, phone_number = :phoneNumber, id_card= :idCard " +
+            " where id = :id", nativeQuery = true)
+    void updateEmployee(@Param("address") String address, @Param("dateOfBirth") LocalDate dateOfBirth,
+                @Param("email") String email, @Param("gender") Boolean gender, @Param("imgUrl") String imgUrl,
+                @Param("name") String name, @Param("phoneNumber") String phoneNumber, @Param("idCard")String idCard, @Param("id") Long id);
 }
--- pawn-shop/src/main/java/com/pawn_shop/service/impl/NewsService.java
+++ pawn-shop/src/main/java/com/pawn_shop/service/impl/NewsService.java
@@ -1,6 +1,26 @@
 package com.pawn_shop.service.impl;
 
+import com.pawn_shop.dto.NewsDto;
+import com.pawn_shop.model.news.News;
+import com.pawn_shop.repository.INewsRepository;
 import com.pawn_shop.service.INewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class NewsService implements INewsService {
+    @Autowired
+    private INewsRepository iNewsRepository;
+
+    @Override
+    public void saveNews(News news) {
+        this.iNewsRepository.saveNews(news.getContent(),news.getImgUrl(),news.getPostingDay(),news.getTitle());
+    }
+
+    @Override
+    public List<News> findAll() {
+        return this.iNewsRepository.findAll();
+    }
 }
